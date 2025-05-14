56e9d995f3a3f3fec852827af7699428e866e097
crazyfaz <crazyfazzziihh@gmail.com>
2025-05-14 18:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+app.listen(3000, () => console.log('Keep-alive server started on port 3000'));
+
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
